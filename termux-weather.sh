468303a5a7452c43ec92ce458144e0854ee2666d
David Lev <42866208+david-lev@users.noreply.github.com>
2021-09-26 15:25:38 +0300

    Separation of the termux-location function

--- termux-weather.sh
+++ termux-weather.sh
@@ -17,9 +17,11 @@ function check_dependencies() {
     [[ ${1} == "termux-location" ]] && pkg='termux-api' || pkg=${1}
     command -v ${1} >/dev/null 2>&1 || (echo -e "${red}The package ${1} is missing. Installing now...${end}\n" && apt install ${pkg} -y)
 }
-
+function get_termux_location() { 
+    location_json=$(timeout ${timeout} termux-location) &&
+    location="$(echo ${location_json} | jq -r '.latitude'),$(echo ${location_json} | jq -r '.longitude')" || get_location
+}
 function get_location() {
-    ([[ ${run_on_termux} ]] && location=$(timeout ${timeout} termux-location)) && (lat=$(echo ${location} | jq -r '.latitude') && lon=$(echo ${location} | jq -r '.longitude')) ||
         echo -e "${yellow}We can not identify your location, select one of the options:\n${green}1:${end} Use my IP address to locate my location (approximate) \n${green}2:${end} Let me enter city name mannualy. \n${green}3:${end}: exit."
     while [[ ${choice} != "1" && ${choice} != "2" && ${choice} != "3" ]]; do
         read choice
@@ -41,5 +43,5 @@ for dep in ${dependencies[@]}; do
 done
 
 # Get weather info:
-get_location
+[[ ${run_on_termux} ]] && get_termux_location || get_location
 get_weather "${location}"
