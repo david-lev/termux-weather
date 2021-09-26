8ca384cceb47a7ff9ce47d6065b7cf9b73ebaa08
David Lev <42866208+david-lev@users.noreply.github.com>
2021-09-26 17:12:04 +0300

    fix

--- termux-weather.sh
+++ termux-weather.sh
@@ -18,11 +18,11 @@ function check_dependencies() {
     command -v ${1} >/dev/null 2>&1 || (echo -e "${red}The package ${1} is missing. Installing now...${end}\n" && apt install ${pkg} -y)
 }
 function get_termux_location() { 
-    (location_json=$(timeout ${timeout} termux-location) &&
-    location="$(echo ${location_json} | jq -r '.latitude'),$(echo ${location_json} | jq -r '.longitude')") || get_location
+    location_json=$(timeout ${timeout} termux-location) &&
+    location="$(echo ${location_json} | jq -r '.latitude'),$(echo ${location_json} | jq -r '.longitude')" || get_location
 }
 function get_location() {
-        echo -e "${yellow}We can not identify your location, select one of the options:\n${green}1:${end} Use my IP address to locate my location (approximate). \n${green}2:${end} Let me enter city name mannualy. \n${green}3:${end} exit."
+    echo -e "${yellow}We can not identify your location, select one of the options:\n${green}1:${end} Use my IP address to locate my location (approximate) \n${green}2:${end} Let me enter city name mannualy. \n${green}3:${end} exit."
     while [[ ${choice} != "1" && ${choice} != "2" && ${choice} != "3" ]]; do
         read choice
     done
@@ -32,9 +32,8 @@ function get_location() {
         3) exit 1 ;;
     esac
 }
-
 function get_weather() {
-    curl -sS --connect-timeout ${timeout} https://wttr.in/${@/ /+}${flags}
+    curl -sS --connect-timeout ${timeout} "https://wttr.in/${@/ /+}${flags}?Q?F"
 }
 
 # Checking dependencies:
